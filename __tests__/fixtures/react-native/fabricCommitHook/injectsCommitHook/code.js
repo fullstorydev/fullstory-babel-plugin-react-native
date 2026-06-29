1755c899993958d5a60407fec3f385ff868ca61f
Ryan Wang <qwang1993@gmail.com>
2026-06-29 19:27:12 -0400

    update react-native, npm audit fix

--- __tests__/fixtures/react-native/fabricCommitHook/injectsCommitHook/code.js
+++ __tests__/fixtures/react-native/fabricCommitHook/injectsCommitHook/code.js
@@ -3,13 +3,6 @@ function getPublicInstance(instance) {
 }
 
 function commitMutationEffectsOnFiber(finishedWork, root) {
-  switch (finishedWork.tag) {
-    case 3:
-      recursivelyTraverseMutationEffects(root, finishedWork);
-      commitReconciliationEffects(finishedWork);
-      break;
-    default:
-      recursivelyTraverseMutationEffects(root, finishedWork);
-      commitReconciliationEffects(finishedWork);
-  }
+  recursivelyTraverseMutationEffects(root, finishedWork);
+  commitReconciliationEffects(finishedWork);
 }
